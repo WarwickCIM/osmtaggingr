aab1759a4aac4f5c6351e69c669a70310d17dc34
Carlos Cámara <ccamara@pm.me>
2026-02-28 21:04:54 +0000

    Update dataset

--- data-raw/tagging_proposals.R
+++ data-raw/tagging_proposals.R
@@ -1,5 +1,5 @@
 ## This code prepares `tagging_proposals` dataset
 
-proposals <- get_tagging_proposals()
+proposals <- get_tagging_proposals(voting_summary = FALSE)
 
-usethis::use_data(tagging_proposals, overwrite = TRUE)
+usethis::use_data(proposals, overwrite = TRUE)
