5c3f787a15544a071dd72aa446b8c8eb74ca4164
Carlos Cámara <ccamara@pm.me>
2026-03-01 22:03:10 +0000

    Update dataset

--- data-raw/tagging_proposals.R
+++ data-raw/tagging_proposals.R
@@ -1,5 +1,6 @@
 ## This code prepares `tagging_proposals` dataset
+library(osmtaggingr)
 
-proposals <- get_tagging_proposals(voting_summary = FALSE)
+proposals <- get_tagging_proposals(max_items = 1000)
 
 usethis::use_data(proposals, overwrite = TRUE)
