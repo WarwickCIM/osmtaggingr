3148e18ad49413a54eb899521c69fdf0ce61f282
Carlos Cámara <carlos.camara@warwick.ac.uk>
2026-03-02 11:30:57 +0000

    Create function to retrieve votes from proposals

--- data-raw/tagging_proposals.R
+++ data-raw/tagging_proposals.R
@@ -1,6 +1,16 @@
 ## This code prepares `tagging_proposals` dataset
 library(osmtaggingr)
 
+
+# Tagging proposals -------------------------------------------------------
+
 proposals <- get_tagging_proposals(max_items = 1000)
 
 usethis::use_data(proposals, overwrite = TRUE)
+
+
+# Proposals votes ---------------------------------------------------------
+
+proposals_votes <- get_proposals_votes(proposals$fullurl)
+
+usethis::use_data(proposals_votes, overwrite = TRUE)
